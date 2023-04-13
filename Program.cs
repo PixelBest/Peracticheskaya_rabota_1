c2273b9d79f9a49a9d54ec24df3cb63f82d9dd2e
Nikita <Nikita@DESKTOP-9314GJ9>
2023-04-13 11:38:51 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Метод Console.WriteLine");
+Console.WriteLine("Hello");
+Console.WriteLine("World");
+Console.WriteLine("!!!");
+Console.WriteLine("Метод Console.Write");
+Console.Write("Hello");
+Console.Write(" World ");
+Console.Write("!!!");
+Console.ReadLine();
+
+for(int i = 1; i < 10; i++)
+{
+    for(int j = 1; j < 10; j++)
+    {
+        Console.Write($"{i * j} \t");
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine();
+}
+Console.ReadLine();
